9422461b95fddcbfa628b89f5be173b4b4f543b5
higaredajra <higaredajra@msoe.edu>
2025-09-25 18:16:21 -0500

    Fix error handling and remove test printing

--- main.cpp
+++ main.cpp
@@ -18,24 +18,25 @@ void initializeArray(int array[][2], int xSize, int minBound) { // Sets up the a
         array[i][0] = minBound + i; // Starts at the minimum bound and sets the first item in each
                                     // sub-array to the value of minBound plus the distance from the min
         array[i][1] = 0; // Initializes the amount of occurrences of each number to 0
-        cout << array[i][0] << " " << array[i][1] << endl; // TODO remove test prints
     }
 }
 
 void readData(int data[][2], int xSize) {
+    int readNumber;
+    cin >> readNumber;
     while (cin) { // Loops until input is empty
-        int readNumber;
-        cin >> readNumber; // Reads a number from input
         for (int i = 0; i < xSize; i++) {
             if (readNumber < data[0][0] || readNumber > data[xSize - 1][0]) { // Verifies that the number read
                                                                               // is within the specified bounds
-                cout << "Error: value of " << readNumber << " is out of range" << endl;
+                cout << "Error: value " << readNumber << " is out of range" << endl;
                 break; // Exits the for loop
-            } else if (readNumber == data[i][0]) { // Looks through the array until it finds its container
+            }
+            if (readNumber == data[i][0]) { // Looks through the array until it finds its container
                 ++data[i][1]; // Increments the count of occurrences
                 break; // Exits the for loop
             }
         }
+        cin >> readNumber; // Reads a number from input
     }
 }
 
@@ -44,7 +45,6 @@ int main() {
     cin >> minBound >> maxBound;
     const int arrayWidth = maxBound - minBound + 1;
     int bars[arrayWidth][2];
-    cout << "Initializing Array..." << endl;
     initializeArray(bars, arrayWidth, minBound);
     readData(bars, arrayWidth);
     return 0;
